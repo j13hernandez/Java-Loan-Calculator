74928da48fb602a11ae31ccbb145b47ac6b0922e
Palma <1hernandezj3@gmail.com>
2022-08-30 02:24:47 -0400

    "Add calculator method"

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -5,20 +5,23 @@ import java.util.Scanner;
 public class LoanCalculator {
 
     /**
-     * TODO: finish out this JavaDoc comment block.
-     * FIXME: WHAT DOES THIS METHOD DO?
+     * This method will take in an amount as a param and 
+     * calculate how much you have remaining if you pay back 
+     * 10% each month
      * 
-     * @param ???
-     * @return ???
+     * @param amount of loan
+     * @return remainder after 3 months
      */
     static int getRemainingAmountIn3Months(int amount) {
-        // TODO: Add your code below
-        System.out.println("getRemainingAmountIn3Months(amount)");
-        return amount;
+        // This is the output 
+        int remainder = amount;
+        for (int i = 0; i < 3; i++) {
+            remainder = remainder - (remainder / 10);
+        }
+        return remainder;
     }
     
     public static void main(String[] args) {
-        System.out.println("getRemainingAmountIn3Months(amount)");
         Scanner scanner = new Scanner(System.in);
         int amount = scanner.nextInt();
         System.out.println(getRemainingAmountIn3Months(amount));
